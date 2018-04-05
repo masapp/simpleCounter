07758a6dfadfa1fa2072d5e2d59cdf7d65036e54
ishikawa_masayuki <ishikawa_masayuki@cyberagent.co.jp>
2018-04-05 10:34:45 +0900

    first commit

---
+++ simpleCounter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  simpleCounter
+//
+//  Created by 石川 雅之 on 2018/04/05.
+//  Copyright © 2018 masapp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
